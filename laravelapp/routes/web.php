9244731a07b89599b8c927ebf7d416dca38fdad7
koki-sasaki <haiku130819@gmail.com>
2022-03-17 15:06:24 +0900

    ３月１７日 chap6

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 //授業
 
+//hello
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post'); 
 Route::get('hello/add', 'HelloController@add');
@@ -28,6 +29,12 @@ Route::post('hello/edit', 'HelloController@update');
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove'); 
 Route::get('hello/show', 'HelloController@show');
+//person
+Route::get('person', 'PersonController@index');
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');
+
+
 
 //実習問題
 
@@ -56,6 +63,28 @@ Route::post('jissyu9', 'Jissyu4_4Controller@post');
 //5-1
 Route::get('jissyu10', 'jissyu5_1Controller@index');
 Route::get('jissyu10/show', 'jissyu5_1Controller@show'); 
+//5-2
+Route::get('jissyu11', 'Jissyu5_2Controller@index');
+Route::get('jissyu11/show', 'Jissyu5_2Controller@show');
+Route::get('jissyu11/add', 'Jissyu5_2Controller@add');
+Route::post('jissyu11/add', 'Jissyu5_2Controller@create');
+Route::get('jissyu11/edit', 'Jissyu5_2Controller@edit');
+Route::post('jissyu11/update', 'Jissyu5_2Controller@update');
+Route::get('jissyu11/del', 'Jissyu5_2Controller@del');
+Route::post('jissyu11/remove', 'Jissyu5_2Controller@remove');
+//5-3
+Route::get('jissyu12', 'Jissyu5_3Controller@index');
+Route::get('jissyu12/show', 'Jissyu5_3Controller@show');
+Route::get('jissyu12/add', 'Jissyu5_3Controller@add');
+Route::post('jissyu12/create', 'Jissyu5_3Controller@create');
+Route::get('jissyu12/edit', 'Jissyu5_3Controller@edit');
+Route::post('jissyu12/update', 'Jissyu5_3Controller@update');
+Route::get('jissyu12/del', 'Jissyu5_3Controller@del');
+Route::post('jissyu12/remove', 'Jissyu5_3Controller@remove');
+//6-1
+Route::get('jissyu13', 'jissyu6_1Controller@index');
+Route::post('jissyu13/find', 'jissyu6_1Controller@find');
+
 
 //効果測定
 
